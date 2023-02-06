83b84cb17b8d2b12b6ddd557beb6f0ab5eeb2f81
Egor <72513616+forgot-sea@users.noreply.github.com>
2023-02-06 16:25:42 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Егор Ермолицкий on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
