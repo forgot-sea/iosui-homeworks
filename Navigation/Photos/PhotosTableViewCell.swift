4c9f25f4684ae05d4055b7fa6ef16f2c4fbda3d5
Egor <72513616+forgot-sea@users.noreply.github.com>
2023-02-27 18:01:11 +0300

    Add pictures for CollectionView and created PhotosTableViewCell.swift

---
+++ Navigation/Photos/PhotosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotosTableViewCell.swift
+//  Navigation
+//
+//  Created by Егор Ермолицкий on 27.02.2023.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
